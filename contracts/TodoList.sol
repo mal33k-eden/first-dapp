3b054c865baaed1c32ed7bcb81f9caafe56946e4
Emm Kwami <edenated@gmail.com>
2021-05-04 00:49:32 -0700

    toggle todolist

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -16,13 +16,23 @@ contract TodoList{
         string content,
         bool completed
     );
-    constructor() public {
+     event TaskCompleted(
+        uint id,
+        bool completed
+    );
+        constructor() public {
         createTask("My first DAPP");
     }
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.isCompleted = !_task.isCompleted;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.isCompleted);
     }
 
 
--- src/app.js
+++ src/app.js
@@ -55,7 +55,12 @@ App = {
         await App.todoList.createTask(content,{from: App.fromAccount})
         window.location.reload();
     },
-
+    toggleCompleted: async (e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId,{from: App.fromAccount})
+        window.location.reload()
+    },
     renderTasks: async () => {
         // Load the total task count from the blockchain
         const taskCount = await App.todoList.taskCount()
